671a10e7c16f4b4e2fad0d39670bc3d0a1908fea
Muhammet Çakmak <muhammetckmk46@gmail.com>
2025-03-12 00:23:07 +0300

    MuhammetC

---
+++ src/test/java/GetlandEstate/pages/searchPage.java
@@ -0,0 +1,44 @@
+package GetlandEstate.pages;
+
+import GetlandEstate.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class searchPage {
+
+    public searchPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath="//button[text()='Rent']")
+    public WebElement rentButton;
+
+    @FindBy(css="input[name='query']")
+    public WebElement searchBox;
+
+    @FindBy(xpath="(//button[@type='button'])[5]")
+    public WebElement searchButton;
+
+    @FindBy(xpath="//button[text()='HOUSE']")
+    public WebElement houseButton;
+
+    @FindBy(xpath="//button[text()='APARTMENT']")
+    public WebElement apartmentButton;
+
+    @FindBy(xpath="//button[text()='OFFICE']")
+    public WebElement officeButton;
+
+    @FindBy(xpath="//button[text()='VILLA']")
+    public WebElement villaButton;
+
+     @FindBy(xpath="//button[text()='LAND']")
+    public WebElement landButton;
+
+    @FindBy(xpath="//button[text()='SHOP']")
+    public WebElement shopButton;
+
+
+
+
+
+}
