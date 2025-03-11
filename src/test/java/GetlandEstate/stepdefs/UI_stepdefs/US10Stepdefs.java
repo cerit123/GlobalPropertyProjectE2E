75ed009f5f78a38991ea81b5e1c9e773f1bf0078
Muhammet Çakmak <muhammetckmk46@gmail.com>
2025-03-11 16:56:45 +0300

    MuhammetC

---
+++ src/test/java/GetlandEstate/stepdefs/UI_stepdefs/US10Stepdefs.java
@@ -0,0 +1,31 @@
+package GetlandEstate.stepdefs.UI_stepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class US10Stepdefs {
+    @Given("sayfaya gidilir")
+    public void sayfayaGidilir() {
+    }
+
+    @And("search box ın üzerindeki sale butonuna tıklanır")
+    public void searchBoxInÜzerindekiSaleButonunaTıklanır() {
+    }
+
+    @And("search box ın altındaki istenilen emlak seçilir")
+    public void searchBoxInAltındakiIstenilenEmlakSeçilir() {
+    }
+
+    @And("search butonuna tıklanır")
+    public void searchButonunaTıklanır() {
+    }
+
+    @And("açılan pencerede seçilen ürünün geldiği görünür")
+    public void açılanPenceredeSeçilenÜrününGeldiğiGörünür() {
+    }
+
+    @Then("sayfa kapatılır")
+    public void sayfaKapatılır() {
+    }
+}
