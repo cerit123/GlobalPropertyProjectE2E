ddf4d52760a8c85d9010c32668219d1bdda7f136
Muhammet Çakmak <muhammetckmk46@gmail.com>
2025-03-12 00:43:26 +0300

    MuhammetC

--- src/test/java/GetlandEstate/pages/SearchPage.java
+++ src/test/java/GetlandEstate/pages/SearchPage.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class searchPage {
+public class SearchPage {
 
-    public searchPage() {
+    public SearchPage() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
     @FindBy(xpath="//button[text()='Rent']")
