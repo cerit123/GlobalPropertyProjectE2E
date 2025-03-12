25c77572470ffac7c6a08e69999f56cf6b6c1036
cerit123 <hafizeozcerit@gmail.com>
2025-03-12 22:09:00 +0000

    gh

---
+++ src/test/java/GetlandEstate/runners/Runner.java
@@ -0,0 +1,20 @@
+package GetlandEstate.runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)//Cucumber ile junitin entegre olmasini ve scenariolarin calistirilmasini saglayan notasyon
+@CucumberOptions(//bu notasyon sayesinde hangi scenariolari calistiracagimizi ve hangi raporlari alacagimizi belirtiriz
+        plugin = {
+                "pretty",//konsol da scenariolar ile ilgili ayrintili bilgi verir
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
+        features = "src/test/resources/features",
+        glue = {"GetlandEstate/stepdefs"},
+        tags = "",
+        dryRun = false
+)
+public class Runner {
+}
---
+++ src/test/java/GetlandEstate/stepdefs/tanım.java
@@ -0,0 +1,6 @@
+package GetlandEstate.stepdefs;
+
+public class tanım {
+    //bu clasta içerisinde feature file larda oluşturulan test caselerin
+    //kodları yazılacak
+}
