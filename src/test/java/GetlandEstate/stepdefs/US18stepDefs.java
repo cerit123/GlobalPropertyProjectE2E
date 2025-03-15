0eab531015888da5691998460a9f7f346ae76adc
cerit123 <hafizeozcerit@gmail.com>
2025-03-15 14:16:45 +0000

    b

--- src/test/java/GetlandEstate/stepdefs/US18stepDefs.java
+++ src/test/java/GetlandEstate/stepdefs/US18stepDefs.java
@@ -20,16 +20,19 @@ public class US18stepDefs {
     @Given("Homepage e tiklanir.")
     public void homepageETiklanir() {
         //ActionsUtils.scrollDown()
-        ReusableMethods.waitForSecond(3);
-        homePage.controlPannelButton.click();
+        loginPage.loginButton.click();
+        ActionsUtils.scrollLeft();
+        ActionsUtils.scrollDown();
+        loginPage.mailButton.sendKeys(ConfigReader.getProperty("manager"));
+        loginPage.passwordButton.sendKeys(ConfigReader.getProperty("password2"));
+        loginPage.login.click();
         dashboardPage.backToSite.click();
-        homePage.profilButton.click();
-        homePage.profilButton.click();
-        myTourRequestsPage.myTourRequests.click();
     }
 
     @When("Istenilen ilan a tiklanir")
     public void ıstenilenIlanATiklanir() {
+        homePage.profilButton.click();
+        myTourRequestsPage.myTourRequests.click();
         myTourRequestsPage.myRequests.click();
         myTourRequestsPage.actionUpdateButton.click();
 
@@ -43,6 +46,7 @@ public class US18stepDefs {
     @And("Schedule a Tour kismindan  tur saati secilir.")
     public void scheduleATourKismindanTurSaatiSecilir() {
         ReusableMethods.ddmVisibleText(myTourRequestsPage.timeBooking,"18:00");
+        myTourRequestsPage.timeBooking.click();
     }
 
     @And("Submit a tour request butonu  tiklanir.")
@@ -56,6 +60,35 @@ public class US18stepDefs {
         Assert.assertTrue(myTourRequestsPage.tourRequestCreatedSuccessfully.isDisplayed());
     }
 
+    //TC02
+
+
+    @Given("Manager olarak giris yapilir.")
+    public void managerOlarakGirisYapilir() {
+        loginPage.loginButton.click();
+        ActionsUtils.scrollLeft();
+        ActionsUtils.scrollDown();
+        loginPage.mailButton.sendKeys(ConfigReader.getProperty("manager"));
+        loginPage.passwordButton.sendKeys(ConfigReader.getProperty("password2"));
+        loginPage.login.click();
+        dashboardPage.backToSite.click();
 
     }
+
+    @And("Profil menusunden  My Tour Requests sekmesi tiklanir")
+    public void profilMenusundenMyTourRequestsSekmesiTiklanir() {
+
+        homePage.profilButton.click();
+        myTourRequestsPage.myTourRequests.click();
+        myTourRequestsPage.myRequests.click();
+    }
+
+    @And("My Responses listesi acilir.")
+    public void myResponsesListesiAcilir() {
+    }
+
+    @Then("My Respopnses listesindeki guest goruntulenebiliyor mu kontrol edilir.")
+    public void myRespopnsesListesindekiGuestGoruntulenebiliyorMuKontrolEdilir() {
+    }
+}
 
