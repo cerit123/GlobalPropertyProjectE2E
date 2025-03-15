df7f7c66abd6fd5d5b798ee950159c0b289f4fa0
Muhammet Çakmak <muhammetckmk46@gmail.com>
2025-03-15 23:13:51 +0300

    us10-us11

--- src/test/java/GetlandEstate/runners/Runner.java
+++ src/test/java/GetlandEstate/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = {"GetlandEstate/stepdefs","GetlandEstate/hooks"},
-        tags = "@US-10m",
+        tags = "@US-11",
         dryRun = false
 )
 public class Runner {
