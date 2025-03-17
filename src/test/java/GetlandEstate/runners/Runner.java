401bb6ff34d544a9705853cb29f492698d7fb7ec
Muhammet Ölünk <molunk@gmail.com>
2025-03-17 11:38:17 +0300

    US01 editlendi

--- src/test/java/GetlandEstate/runners/Runner.java
+++ src/test/java/GetlandEstate/runners/Runner.java
@@ -1,4 +1,5 @@
 package GetlandEstate.runners;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
@@ -12,25 +13,8 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml"
         },
         features = "src/test/resources/features",
-<<<<<<< HEAD
-<<<<<<< HEAD
-        glue = {"GetlandEstate/stepdefs"},
-<<<<<<< HEAD
-        tags = "@Manager",
-=======
-        tags = "@US-17",
-=======
-        glue = {"GetlandEstate/stepdefs","GetlandEstate/hooks"},
-        tags = "",
->>>>>>> fcc1a42614fb10302cfa0d28fa1f76f432abaf77
->>>>>>> master
-=======
-
-        glue = {"GetlandEstate/stepdefs"},
-        tags = "@US-17",
-
-
->>>>>>> 26d1763fcfb43df0c09ac1ed201a1a54e5768d47
+        glue = {"GetlandEstate/stepdefs", "GetlandEstate/hooks"},
+        tags = "@US01",
         dryRun = false
 )
 public class Runner {
